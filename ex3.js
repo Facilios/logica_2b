a6ff89a5fc901707cceb3ffad85bef299b0c2747
FaFa Gab's <fabriciofrei01@gmail.com>
2024-04-25 16:46:12 +0000

    metade feita

---
+++ ex3.js
@@ -0,0 +1,11 @@
+const ler = require("prompt-sync")()
+
+let a = Number(ler(Digite o valor de A)) 
+let b = Number(ler(Digite o valor de b))
+let c = Number(ler(Digite o valor de c))
+
+let areaTriangulo = (a * c) / 2
+let areaCirculo = c * 3.14
+let areaTrapezio = ((a + b) * c) / 2
+let areaQuadrado = b * b
+let areaReangulo = a * b
