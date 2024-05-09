f8630725182facad215a69bcf58963da35258ff4
FaFa Gab's <fabriciofrei01@gmail.com>
2024-05-09 13:00:22 +0000

    implementaçao de melhorias (math.pow e math.pi)

--- ex3.js
+++ ex3.js
@@ -5,13 +5,13 @@ let b = Number(ler("Digite o valor de b: "))
 let c = Number(ler("Digite o valor de c: "))
 
 let areaTriangulo = (a * c) / 2
-let areaCirculo = 3.14159 * (c * c)
+let areaCirculo = Math.PI * Math.pow( c, 2 )
 let areaTrapezio = ((a + b) * c) / 2
 let areaQuadrado = b * b
 let areaReangulo = a * b
 
-console.log(`TRIANGULO ${areaTriangulo.toFixed(3)}`);
-console.log(`CIRCULO ${areaCirculo.toFixed(3)}`);
-console.log(`TRAPEZIO ${areaTrapezio.toFixed(3)}`);
-console.log(`QUADRADO ${areaQuadrado.toFixed(3)}`);
-console.log(`RETANGULO ${areaReangulo.toFixed(3)}`);
+console.log(`TRIANGULO: ${areaTriangulo.toFixed(3)}`);
+console.log(`CIRCULO: ${areaCirculo.toFixed(3)}`);
+console.log(`TRAPEZIO: ${areaTrapezio.toFixed(3)}`);
+console.log(`QUADRADO: ${areaQuadrado.toFixed(3)}`);
+console.log(`RETANGULO: ${areaReangulo.toFixed(3)}`)
